dbd944958fe84cf87a16b7b3355bc132ca35f0c9
Chenyang Liu <chenyl@microsoft.com>
2025-01-09 16:14:08 +0800

    Add integration

---
+++ Tests/SignalRClientTests/IntegrationTests.swift
@@ -0,0 +1,60 @@
+
+import XCTest
+@testable import SignalRClient
+
+class IntegrationTests: XCTestCase {
+    private var url: String?
+
+    override func setUpWithError() throws {
+        // guard let url = ProcessInfo.processInfo.environment["SIGNALR_INTEGRATION_TEST_URL"] else {
+        //     throw XCTSkip("Skipping integration tests because SIGNALR_INTEGRATION_TEST_URL is not set.")
+        // }
+        let url = "http://localhost:8080/test"
+        self.url = url
+    }
+
+    func testConnect() async throws {
+#if os(Linux)
+        let transports: [HttpTransportType] = [.longPolling]
+#else
+        let transports: [HttpTransportType] = [.webSockets, .serverSentEvents, .longPolling]
+#endif
+        let hubProtocols: [HubProtocolType] = [.json, .messagePack]
+
+        for (transport, hubProtocol) in zip(transports, hubProtocols) {
+            try await testConnectCore(transport: transport, hubProtocol: hubProtocol)
+        }
+    }
+
+    private func testConnectCore(transport: HttpTransportType, hubProtocol: HubProtocolType) async throws {
+        let connection = HubConnectionBuilder()
+            .withUrl(url: url!, transport: transport)
+            .withHubProtocol(hubProtocol: hubProtocol)
+            .withLogLevel(logLevel: .debug)
+            .build()
+
+        try await connection.start()
+    }
+
+    // func testInvoke() throws {
+    //     let expectation = self.expectation(description: "Invoke")
+    //     connection?.invoke(method: "TestMethod", "TestParam") { error in
+    //         if error == nil {
+    //             expectation.fulfill()
+    //         }
+    //     }
+    //     wait(for: [expectation], timeout: 10.0)
+    // }
+
+    // func testStream() throws {
+    //     let expectation = self.expectation(description: "Stream")
+    //     let stream = connection?.stream(method: "TestStreamMethod", "TestParam")
+    //     stream?.observe { (value: Int?) in
+    //         if value == nil {
+    //             expectation.fulfill()
+    //         }
+    //     }
+    //     stream?.start()
+    //     wait(for: [expectation], timeout: 10.0)
+    // }
+}
